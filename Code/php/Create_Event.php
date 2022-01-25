df193ff046ef7533e63fb8488557c5db07c623ad
Elliot Stevenson <91467755+b0gu5@users.noreply.github.com>
2022-01-25 16:59:51 +0000

    Update Create_Event.php
    Added variables that will take user input from an HTML form to the SQL Insert statement.
    Has not been tested yet as there is no website or database to test on yet but should still work.
    Will need to change column names for when the database is set up

--- Code/php/Create_Event.php
+++ Code/php/Create_Event.php
@@ -11,14 +11,27 @@
 <?php
 
 // Include the database connection
-include "db.php";
+include "dbconnect.php";
 // Check that a form has been submitted
 if( isset($_POST['submit']) ){
-$stmt = $mysql->prepare("INSERT INTO un_goals_table (UN_Sustainable_Development_Goal, About_The_Goal, Things_To_Do, Examples_In_Dundee, Useful_Information, Website) VALUES ("DummyGoal", "This goal is a dummy", "you can do dummy things", "Dummy location in Dundee", "Dummy Information", "Dummy Website Link");
-//$stmt->bindParam(":UserInput", $userinput);
-//$userinput = $_POST['query'];
-$stmt->execute();
+$inputGoalTitle = $_POST['goalTitle'];
+$inputGoalAbout = $_POST['goalAbout'];
+$inputGoalToDos = $_POST['goalToDos'];
+$inputGoalsInDundee = $_POST['goalsInDundee'];
+$inputUsefulInformation = $_POST['usefulInformation'];
+$inputWebsite = $_POST['website'];
+//Need to update column names for real table
+$createStmt = $mysql->prepare("INSERT INTO un_goals_table (UN_Sustainable_Development_Goal, About_The_Goal, Things_To_Do, Examples_In_Dundee, Useful_Information, Website) VALUES ($inputGoalTitle, $inputGoalAbout, $inputGoalToDos, $inputGoalsInDundee, $inputUsefulInformation, $inputWebsite);
+$createStmt->execute();
 $result = $stmt->fetchAll();
+
+if($result == false){
+   echo "Event creation failed please try again";
+}else{
+   $readStmt = $mysql->prepare("SELECT * FROM un_goals_table");
+   $readStmt->execute();
+}
+
 } else {
 // Error handling
 
@@ -27,7 +40,6 @@ $result = [];
 
 }
 
-//echo $userinput;
 
 foreach($result as $row) {
 echo "<td>".$row['UN Sustainable Development Goal']."</td>";
