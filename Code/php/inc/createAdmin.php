bd429ae0cac071a5661ed6354a09c67ff20d7eff
Khaelem Watt <kajwatt@dundee.ac.uk>
2022-01-26 21:10:07 +0000

    Commented createAdmin.php

--- Code/php/inc/createAdmin.php
+++ Code/php/inc/createAdmin.php
@@ -28,20 +28,24 @@
         </div>
     </div>
     <script>
+        //Get references to html elements
         const form = document.getElementById("login");
         button = document.getElementById("submitButton");
         usernameInput = document.getElementById("username");
         passwordInput = document.getElementById("password");
 
+        //Will activate when the animation ends. This is to set it back to default
         button.addEventListener("animationend", function(){
             button.classList.remove("animate__headShake");
             button.classList.remove("btn-danger");
         });
 
+        //To be removed
         function accept(){
             console.log("Accept function");
         }
 
+        //If rejected, animate the button
         function reject(){
             console.log("Reject function");
             button.classList.add("animate__headShake");
@@ -49,23 +53,37 @@
         }
     </script>
     <?php 
+        //Set error display stuff
         ini_set('display_errors', 1);
         ini_set('display_startup_errors', 1);
         error_reporting(E_ALL);
+
+        //Include databse connection
         include("dbconnect.php");
 
+        //Checks if the post values are set (i.e. have been posted) and arent empty
         $usernameSet = isset($_POST['username']) && !empty($_POST['username']);
         $passwordSet = isset($_POST['password']) && !empty($_POST['password']);
 
+        //If both username and password are set and not empty we know the form has been submitted
         if($usernameSet && $passwordSet){
+
+            //Store the values
             $username = $_POST['username'];
             $password = $_POST['password'];
 
+            //Form the sql query for selecting the password for the username provided
             $sql = "INSERT INTO `user` (`username`, `password`, `level`) VALUES (";
             $sql = sprintf("%s '%s', '%s', 1);", $sql, $username, $password);
 
+            //
+            //NEED TO COVER CASE WHERE USERNAME ALREADY EXISTS IN THE DATABASE
+            //
+
+            //Send database our query
             $db->query($sql);     
             
+            //Debug Statements
             console($sql);
         }        
 
