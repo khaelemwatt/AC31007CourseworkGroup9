6d6abdfb773e586e9b826a9424689234f7c2e279
Khaelem Watt <kajwatt@dundee.ac.uk>
2022-01-26 14:28:13 +0000

    Added login javascript files

--- Code/php/inc/login.php
+++ Code/php/inc/login.php
@@ -32,16 +32,7 @@
 
     ?>
 
-    <script>
-        
-    function submit(e){
-        e.preventDefault();
-        console.log("Hello Test");
-        Document.getElementById("login").submit();
-        return false;
-    }
-
-    </script>
+    <script src="../JavaScript/validateLogin.js"></script>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
