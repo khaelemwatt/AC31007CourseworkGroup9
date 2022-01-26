a9d0669e37baba208074a3b48bf1fb700f1c0dcd
Khaelem Watt <kajwatt@dundee.ac.uk>
2022-01-26 21:05:47 +0000

    commented login.php

--- Code/php/inc/login.php
+++ Code/php/inc/login.php
@@ -33,15 +33,18 @@
         usernameInput = document.getElementById("username");
         passwordInput = document.getElementById("password");
 
+        //Will activate when the animation ends. This is to set it back to default
         button.addEventListener("animationend", function(){
             button.classList.remove("animate__headShake");
             button.classList.remove("btn-danger");
         });
 
+        //To be removed
         function accept(){
             console.log("Accept function");
         }
 
+        //If rejected, animate the button
         function reject(){
             console.log("Reject function");
             button.classList.add("animate__headShake");
@@ -49,35 +52,56 @@
         }
     </script>
     <?php 
+        //Set error display stuff
         ini_set('display_errors', 1);
         ini_set('display_startup_errors', 1);
         error_reporting(E_ALL);
+
+        //Include databse connection
         include("dbconnect.php");
 
+        //Checks if the post values are set (i.e. have been posted) and arent empty
         $usernameSet = isset($_POST['username']) && !empty($_POST['username']);
         $passwordSet = isset($_POST['password']) && !empty($_POST['password']);
 
+        //if both username and password are set and not empty we know the form has been submitted
         if($usernameSet && $passwordSet){
+
+            //Store the values
             $username = $_POST['username'];
             $password = $_POST['password'];
 
+            //Form the sql query for selecting the password for the username provided
             $sql = "SELECT `password`, `level` FROM `user` WHERE";
             $sql = sprintf("%s `username`='%s';", $sql, $username);
 
+            //
+            //NEED TO COVER CASE WHERE USERNAME TYPED IS NOT IN THE DATABASE
+            //
+
+            //Send the database our query and store the result
             $result = $db->query($sql);
             $row = $result->fetch_array();
             
+            //Debug statements
             console($sql);
             console($row['password']);
 
+            //Check if the password providedd matches the one in the database
             if($password == $row['password']){
+                //If it matches we need to check what user we are dealing with
                 if($row['level'] == 0){
+                    //If user has level 0 they are super admin and can create other admins
+                    //Redirect this user to the page to create admins
                     echo '<script> window.location.href = "/createAdmin" </script>';
                 }else{
+                    //If user has anything else (level 1) they are normal admin who can
+                    //add/edit/remove events. Redirect them to some page
                     echo '<script> window.location.href = "/api/allEvents" </script>';
                 }
                 
             }else{
+                //If password doesnt match, reject this login attempt
                 console("Rejected");
                 echo '<script>reject();</script>';
             }            
