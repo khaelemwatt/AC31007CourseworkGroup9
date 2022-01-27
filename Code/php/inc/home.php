483efb025571e844bd7a1db9a0bf7f0bd608be2f
Khaelem Watt <kajwatt@dundee.ac.uk>
2022-01-27 13:04:34 +0000

    fixed css paths after file reorganisation

--- Code/php/inc/home.php
+++ Code/php/inc/home.php
@@ -3,9 +3,9 @@
 <head>
 <meta charset='utf-8'><meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
 
-<link href='CSS\bootstrap.min.css' rel='stylesheet' type='text/css'>
-<link href='CSS\Colours.css' rel='stylesheet' type='text/css'>
-<link href='CSS\Goals.css' rel='stylesheet' type='text/css'>
+<link href='../../CSS\bootstrap.min.css' rel='stylesheet' type='text/css'>
+<link href='../../CSS\Colours.css' rel='stylesheet' type='text/css'>
+<link href='../../CSS\Goals.css' rel='stylesheet' type='text/css'>
 <title>UN Goals</title>
 
 </head>
