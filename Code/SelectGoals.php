2a5cd62b3c17ae9d482c9ff7d38a718581507f3b
jack morrison <bobimnot123@gmail.com>
2022-01-27 11:19:49 +0000

    forgot quote marks

--- Code/SelectGoals.php
+++ Code/SelectGoals.php
@@ -9,7 +9,7 @@
 <title>UN Goals</title>
 
 </head>
-<?php echo <body >
+<?php echo "<body >
   <div class='pos-f-t '>
     <div class='collapse' id='navbarToggleExternalContent'>
       <div class='blackB p-4'>
@@ -267,7 +267,7 @@
               </div>
             </div>
           </div>
-</body>
+</body>";
 ?>
 <script src='https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js' integrity='sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj' crossorigin='anonymous'></script>
 <script src='js/bootstrap.bundle.js' ></script>
