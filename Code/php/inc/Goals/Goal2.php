2bf650722d83aa53a2d313d810b4a529c295f0f8
jack morrison <bobimnot123@gmail.com>
2022-01-27 19:08:33 +0000

    Added goals.php

---
+++ Code/php/inc/Goals/Goal2.php
@@ -0,0 +1,85 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<link href="..\CSS\bootstrap.min.css" rel="stylesheet" type="text/css">
+<link href="..\CSS\Colours.css" rel="stylesheet" type="text/css">
+<link href="..\CSS\Goals.css" rel="stylesheet" type="text/css">
+<title>End Poverty</title>
+</head>
+<?php echo '
+<body class="">
+      <div class="pos-f-t ">
+        <div class="collapse" id="navbarToggleExternalContent">
+          <div class="blackB p-4">
+            <h4 class="text-white"></h4>
+            <span class="text-muted"></span>
+          </div>
+        </div>
+        <nav class="navbar navbar-dark blackB ">
+          <a class="btn Viridian_Green btn-lg" href="../SelectGoals.html" role="button">Return</a>
+          <img class="Logo" src="../Images/SDGlogo.jpg" alt="SDG">
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          
+        </nav>
+      </div>
+    <div class="container-fluid GoalTitle">
+        <H1 class="display-5 Viridian_Green">Goal 2: ZERO HUNGER<br><small class=" Yellow_Green">End hunger, achieve food security and improved nutrition and promote sustainable agriculture</small></H1>
+    </div>
+    <div class="card  justify-content-center Rose_RedB HowToHelp" >
+        
+          <h5 class=" display-5">How To Help</h5>
+          <p class="lead">Buy local and in-season food.
+          Try growing some of your own food
+          Donlt waste food
+          Buy "funny fruit" many fruits and vegetables are thrown out because their size, shape, or color are not “right”. Buying these perfectly good funny fruit, utilizes food that might otherwise go to waste.
+          Consume less meat, eat more plant based. 
+          </p> 
+    </div>
+    <div class="container-fluid justify-content-center GoalTitle" >
+        <H3 class="display-5 Viridian_Green">Local Charities</H1>
+    </div>
+      <div class="row justify-content-center ">
+        <div class="card Yellow_GreenB EID" style="max-width: 30%;">
+            <h4 >Dundee Community Fridge (Gate Church Carbon Saving Project)</h4>
+            <hr class="my-4">
+            <p>The Community Fridge at Gate Church is a communal place where surplus food is shared, by local businesses and individuals. The community fridge helps to cut food waste, build bonds within the community and re-distribute good quality food. Although the focus is on  food waste, the Community Fridge also helps to reduce food poverty in Dundee. </p>
+            <p class="lead">
+              <a class="btn Viridian_Green btn-lg" href="https://www.gatechurch.co.uk/carbonsavingproject" role="button">Learn more</a>
+            </p>
+          </div>
+          <div class="card Yellow_GreenB EID" style="max-width: 30%;">
+            <h4 >Dundee and Angus Foodbank</h4>
+            <hr class="my-4">
+            <p>The Dundee and Angus Foodbank provide emergency food parcels for those in need and support people in crisis. It is part of a nationwide network of foodbanks, supported by The Trussell Trust, working to combat poverty and hunger across the UK. Locations both in Dundee and Angus. </p>
+            <p class="lead">
+              <a class="btn Viridian_Green btn-lg" href="https://dundeeandangus.foodbank.org.uk/" role="button">Learn more</a>
+            </p>
+          </div>
+          <div class="card Yellow_GreenB EID" style="max-width: 30%;">
+            <h4 >Taught by Muhammad</h4>
+            <hr class="my-4">
+            <p>Taught By Muhammad is an outreach project aimed at building an understanding between Muslims and the wider community. The project also helps tackle social injustices within Dundee. They have several projects including curry kitchens where they cook for the homeless, a foodbank and schools outreach. 
+            *Potential for presentations in schools*</p>
+            <p class="lead">
+              <a class="btn Viridian_Green btn-lg" href="http://www.taughtbymuhammad.com/" role="button">Learn more</a>
+            </p>
+          </div>
+          <div class="card Yellow_GreenB EID" style="max-width: 30%;">
+            <h4 >FareShare Dundee</h4>
+            <hr class="my-4">
+            <p>FareShare Dundee collects/receives surplus quality food. The food is distributed to community organisations providing residential and day services for disadvantaged groups of people in Dundee, Perth & Kinross and Angus.</p>
+            <p class="lead">
+              <a class="btn Viridian_Green btn-lg" href="http://www.taughtbymuhammad.com/" role="button">Learn more</a>
+            </p>
+          </div>
+    </div>
+</body>
+'?>
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="../js/bootstrap.bundle.min.js"></script>
+
+</html>
---
+++ Code/php/inc/Goals/Goal6.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html lang='en'>
+<head>
+<meta charset='utf-8'><meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
+
+<link href='..\CSS\bootstrap.min.css' rel='stylesheet' type='text/css'>
+<link href='..\CSS\Colours.css' rel='stylesheet' type='text/css'>
+<link href='..\CSS\Goals.css' rel='stylesheet' type='text/css'>
+<title>End Poverty</title>
+</head>
+<?php echo "
+<body class=''>
+      <div class='pos-f-t '>
+        <div class='collapse' id='navbarToggleExternalContent'>
+          <div class='blackB p-4'>
+            <h4 class='text-white'></h4>
+            <span class='text-muted'></span>
+          </div>
+        </div>
+        <nav class='navbar navbar-dark blackB '>
+          <a class='btn Viridian_Green btn-lg' href='../SelectGoals.html' role='button'>Return</a>
+          <img class='Logo' src='../Images/SDGlogo.jpg' alt='SDG'>
+          <button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarToggleExternalContent' aria-controls='navbarToggleExternalContent' aria-expanded='false' aria-label='Toggle navigation'>
+            <span class='navbar-toggler-icon'></span>
+          </button>
+          
+        </nav>
+      </div>
+    <div class='container-fluid GoalTitle'>
+        <H1 class='display-5 Viridian_Green'>Goal 6:Clean Water And Sanitation<br><small class=' Yellow_Green'>Ensure availability and sustainable management of water and sanitation for all</small></H1>
+    </div>
+    <div class='card  justify-content-center Rose_RedB HowToHelp' >
+        
+          <h5 class=' display-5'>How To Help</h5>
+          <p class='lead'>Awareness campaigns
+          Don't overuse water
+          Organise a clean up project for rivers and beaches
+          </p> 
+    </div>
+    <div class='container-fluid justify-content-center GoalTitle' >
+        <H3 class='display-5 Viridian_Green'>Local Charities</H1>
+    </div>
+      <div class='row justify-content-center '>
+        <div class='card Yellow_GreenB EID' style='max-width: 30%;'>
+            <h4 >Scottish Water</h4>
+            <hr class='my-4'>
+            <p>St Leonard Park Regeneration - Scottish Water, NatureScot and SEPA are working with Dundee City Council to develop and improve the way storm water is managed and reduce flood risk in the St Mary's area of Dundee. 
+            Water mains investment - Dundee Laird Street. 1km of new water main was installed to improve the water network serving Dundee's Laird Street.</p>
+            <p class='lead'>
+              <a class='btn Viridian_Green btn-lg' href='https://www.scottishwater.co.uk/in-your-area/investments-in-your-area/st-marys-drainage' role='button'>Learn more</a>
+            </p>
+          </div>
+    </div>
+</body>
+"?>
+<script src='https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js' integrity='sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj' crossorigin='anonymous'></script>
+<script src='../js/bootstrap.bundle.min.js'></script>
+
+</html> 
