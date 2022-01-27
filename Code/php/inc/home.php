9e87c11dddc99a85e7e3d753eda94df2c31a63c6
Khaelem Watt <kajwatt@dundee.ac.uk>
2022-01-27 13:54:55 +0000

    Moved images into the inc folder

--- Code/php/inc/home.php
+++ Code/php/inc/home.php
@@ -18,7 +18,7 @@
       </div>
     </div>
     <nav class='navbar navbar-dark blackB '>
-      <img class='Logo' src='/Code/Images/SDGlogo.jpg' alt='SDG'>
+      <img class='Logo' src='Images/SDGlogo.jpg' alt='SDG'>
       <button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarToggleExternalContent' aria-controls='navbarToggleExternalContent' aria-expanded='false' aria-label='Toggle navigation'>
         <span class='navbar-toggler-icon'></span>
       </button>
