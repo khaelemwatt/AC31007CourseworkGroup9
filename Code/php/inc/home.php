680d9b346bf9ce9ff8542b480c1fdbd73f6a9f19
Khaelem Watt <55397210+khaelemwatt@users.noreply.github.com>
2022-01-27 14:27:50 +0000

    changed path

--- Code/php/inc/home.php
+++ Code/php/inc/home.php
@@ -18,7 +18,7 @@
       </div>
     </div>
     <nav class='navbar navbar-dark blackB '>
-      <img class='Logo' src='SDGlogo.jpg' alt='SDG'>
+      <img class='Logo' src='C:\home\site\wwwroot\SDGlogo.jpg' alt='SDG'>
       <button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarToggleExternalContent' aria-controls='navbarToggleExternalContent' aria-expanded='false' aria-label='Toggle navigation'>
         <span class='navbar-toggler-icon'></span>
       </button>
