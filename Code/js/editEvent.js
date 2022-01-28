a06de8607d4e1cf3ac9edc3beb09bf98e441633b
Struan Robertson <struanrobertson@protonmail.com>
2022-01-28 15:11:06 +0000

    get data from api

--- Code/js/editEvent.js
+++ Code/js/editEvent.js
@@ -1,6 +1,10 @@
 //example response, cross site scripting rules mean that you cant 
 // need to add escape chars to api
-var jsonResponse = '[{"0":"1","EventId":"1","1":"Litter Clean-up","Name":"Litter Clean-up","2":"Come join us clean up Dundee\'s litter!","Info":"Come join us clean up Dundee\'s litter!","3":"2022-02-01","Date":"2022-02-01","4":"Dundee","Location":"Dundee","5":null,"goalID":null},{"0":"2","EventId":"2","1":"Dundee feeds the needy","Name":"Dundee feeds the needy","2":"We will provide a hot soup stand free of charge for anyone who might need it","Info":"We will provide a hot soup stand free of charge for anyone who might need it","3":"2022-02-10","Date":"2022-02-10","4":"Dundee","Location":"Dundee","5":null,"goalID":null}]';
+var xmlHttp = new XMLHttpRequest();
+xmlHttp.open( "GET", 'https://group9agilewebapp.azurewebsites.net/api/allEvents', false );
+xmlHttp.send( null );
+var jsonResponse = xmlHttp.responseText;
+
 var response = JSON.parse(jsonResponse);
 
 var listGroup = document.getElementById("eventsList");
