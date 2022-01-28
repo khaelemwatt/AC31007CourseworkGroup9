20a551714203257ad436dd084032ad9dcc319166
archiemilne <archie@prorex.net>
2022-01-28 15:19:07 +0000

    go go powerrangers

--- Code/php/inc/home.php
+++ Code/php/inc/home.php
@@ -257,7 +257,6 @@
   var goal1Card = document.getElementById("goal1card");
   goal1.addEventListener("mouseover", function(event){
     console.log("Mouse over");
-    goal1card.innerHTML = "<h1>No Poverty</h1>";
     goal1card.innerHTML = "<p>End Poverty in all its forms everywhere</p>";
     goal1card.innerHTML += "<a href='Goals/Goal1.php' class='btn MoreInfo mt-auto'>More Information</a>";
   });
@@ -268,7 +267,6 @@
     var goal1 = document.getElementById("goal1");
     goal1.addEventListener("mouseover", function(event){
       console.log("Mouse over");
-      goal1card.innerHTML = "<h1>No Poverty</h1>";
       goal1card.innerHTML = "<p>End Poverty in all its forms everywhere</p>";
       goal1card.innerHTML += "<a href='Goals/Goal1.php' class='btn MoreInfo mt-auto'>More Information</a>";
     });
@@ -290,6 +288,291 @@
       console.log("Mouse over");
       goal2card.innerHTML = "<p>End hunger, achieve food security and improved nutrition and promote sustainable agriculture</p>";
       goal2card.innerHTML += "<a href='Goals/Goal2.php' class='btn MoreInfo align-self-end'>More Information</a>";
+    });
+  });
+
+  var goal3 = document.getElementById("goal3");
+  var goal3Card = document.getElementById("goal3card");
+  goal3.addEventListener("mouseover", function(event){
+    console.log("Mouse over");
+    goal3card.innerHTML = "<p>Ensure healthy lives and promote well-being for all at all ages</p>";
+    goal3card.innerHTML += "<a href='Goals/Goal3.php' class='btn MoreInfo mt-auto'>More Information</a>";
+  });
+
+  goal3card.addEventListener("mouseleave", function(event){
+    console.log("Mouse out");
+    goal3card.innerHTML = "<img class='card-img-top img-fluid' src='Code/php/inc/Images/Goal3.jpg' alt='Goal3' id='goal3'>";
+    var goal3 = document.getElementById("goal3");
+    goal3.addEventListener("mouseover", function(event){
+      console.log("Mouse over");
+      goal3card.innerHTML = "<p>Ensure healthy lives and promote well-being for all at all ages</p>";
+      goal3card.innerHTML += "<a href='Goals/Goal3.php' class='btn MoreInfo mt-auto'>More Information</a>";
+    });
+  });
+
+  var goal4 = document.getElementById("goal4");
+  var goal4Card = document.getElementById("goal4card");
+  goal4.addEventListener("mouseover", function(event){
+    console.log("Mouse over");
+    goal4card.innerHTML = "<p>Ensure inclusive and equitable quality education and promote lifelong learning opportunities for all</p>";
+    goal4card.innerHTML += "<a href='Goals/Goal4.php' class='btn MoreInfo mt-auto'>More Information</a>";
+  });
+
+  goal4card.addEventListener("mouseleave", function(event){
+    console.log("Mouse out");
+    goal4card.innerHTML = "<img class='card-img-top img-fluid' src='Code/php/inc/Images/Goal4.jpg' alt='Goal4' id='goal4'>";
+    var goal4 = document.getElementById("goal4");
+    goal4.addEventListener("mouseover", function(event){
+      console.log("Mouse over");
+      goal4card.innerHTML = "<p>Ensure inclusive and equitable quality education and promote lifelong learning opportunities for all</p>";
+      goal4card.innerHTML += "<a href='Goals/Goal4.php' class='btn MoreInfo mt-auto'>More Information</a>";
+    });
+  });
+
+  var goal5 = document.getElementById("goal5");
+  var goal5Card = document.getElementById("goal5card");
+  goal5.addEventListener("mouseover", function(event){
+    console.log("Mouse over");
+    goal5card.innerHTML = "<p>Achieve gender equality and empower all women and girls</p>";
+    goal5card.innerHTML += "<a href='Goals/Goal5.php' class='btn MoreInfo mt-auto'>More Information</a>";
+  });
+
+  goal5card.addEventListener("mouseleave", function(event){
+    console.log("Mouse out");
+    goal5card.innerHTML = "<img class='card-img-top img-fluid' src='Code/php/inc/Images/Goal5.jpg' alt='Goal5' id='goal5'>";
+    var goal5 = document.getElementById("goal5");
+    goal5.addEventListener("mouseover", function(event){
+      console.log("Mouse over");
+      goal5card.innerHTML = "<p>Achieve gender equality and empower all women and girls</p>";
+      goal5card.innerHTML += "<a href='Goals/Goal5.php' class='btn MoreInfo mt-auto'>More Information</a>";
+    });
+  });
+  
+  var goal6 = document.getElementById("goal6");
+  var goal6Card = document.getElementById("goal6card");
+  goal6.addEventListener("mouseover", function(event){
+    console.log("Mouse over");
+    goal6card.innerHTML = "<p>Ensure availability and sustainable management of water and sanitation for all</p>";
+    goal6card.innerHTML += "<a href='Goals/Goal6.php' class='btn MoreInfo mt-auto'>More Information</a>";
+  });
+
+  goal6card.addEventListener("mouseleave", function(event){
+    console.log("Mouse out");
+    goal6card.innerHTML = "<img class='card-img-top img-fluid' src='Code/php/inc/Images/Goal6.jpg' alt='Goal6' id='goal6'>";
+    var goal6 = document.getElementById("goal6");
+    goal6.addEventListener("mouseover", function(event){
+      console.log("Mouse over");
+      goal6card.innerHTML = "<p>Ensure availability and sustainable management of water and sanitation for all</p>";
+      goal6card.innerHTML += "<a href='Goals/Goal6.php' class='btn MoreInfo mt-auto'>More Information</a>";
+    });
+  });
+  
+  var goal7 = document.getElementById("goal7");
+  var goal7Card = document.getElementById("goal7card");
+  goal7.addEventListener("mouseover", function(event){
+    console.log("Mouse over");
+    goal7card.innerHTML = "<p>Ensure access to affordable, reliable, sustainable and modern energy for all</p>";
+    goal7card.innerHTML += "<a href='Goals/Goal7.php' class='btn MoreInfo mt-auto'>More Information</a>";
+  });
+
+  goal7card.addEventListener("mouseleave", function(event){
+    console.log("Mouse out");
+    goal7card.innerHTML = "<img class='card-img-top img-fluid' src='Code/php/inc/Images/Goal7.jpg' alt='Goal7' id='goal7'>";
+    var goal7 = document.getElementById("goal7");
+    goal7.addEventListener("mouseover", function(event){
+      console.log("Mouse over");
+      goal7card.innerHTML = "<p>Ensure access to affordable, reliable, sustainable and modern energy for all</p>";
+      goal7card.innerHTML += "<a href='Goals/Goal7.php' class='btn MoreInfo mt-auto'>More Information</a>";
+    });
+  });
+
+  var goal8 = document.getElementById("goal8");
+  var goal8Card = document.getElementById("goal8card");
+  goal8.addEventListener("mouseover", function(event){
+    console.log("Mouse over");
+    goal8card.innerHTML = "<p>Promote sustained, inclusive and sustainable economic growth, full and productive employment and decent work for all</p>";
+    goal8card.innerHTML += "<a href='Goals/Goal8.php' class='btn MoreInfo mt-auto'>More Information</a>";
+  });
+
+  goal8card.addEventListener("mouseleave", function(event){
+    console.log("Mouse out");
+    goal8card.innerHTML = "<img class='card-img-top img-fluid' src='Code/php/inc/Images/Goal8.jpg' alt='Goal8' id='goal8'>";
+    var goal8 = document.getElementById("goal8");
+    goal8.addEventListener("mouseover", function(event){
+      console.log("Mouse over");
+      goal8card.innerHTML = "<p>Promote sustained, inclusive and sustainable economic growth, full and productive employment and decent work for all</p>";
+      goal8card.innerHTML += "<a href='Goals/Goal8.php' class='btn MoreInfo mt-auto'>More Information</a>";
+    });
+  });
+
+  var goal9 = document.getElementById("goal9");
+  var goal9Card = document.getElementById("goal9card");
+  goal9.addEventListener("mouseover", function(event){
+    console.log("Mouse over");
+    goal9card.innerHTML = "<p>Build resilient infrastructure, promote inclusive and sustainable industrialization and foster innovation</p>";
+    goal9card.innerHTML += "<a href='Goals/Goal9.php' class='btn MoreInfo mt-auto'>More Information</a>";
+  });
+
+  goal9card.addEventListener("mouseleave", function(event){
+    console.log("Mouse out");
+    goal9card.innerHTML = "<img class='card-img-top img-fluid' src='Code/php/inc/Images/Goal9.jpg' alt='Goal9' id='goal9'>";
+    var goal9 = document.getElementById("goal9");
+    goal9.addEventListener("mouseover", function(event){
+      console.log("Mouse over");
+      goal9card.innerHTML = "<p>Build resilient infrastructure, promote inclusive and sustainable industrialization and foster innovation</p>";
+      goal9card.innerHTML += "<a href='Goals/Goal9.php' class='btn MoreInfo mt-auto'>More Information</a>";
+    });
+  });
+  
+  var goal10 = document.getElementById("goal10");
+  var goal10Card = document.getElementById("goal10card");
+  goal10.addEventListener("mouseover", function(event){
+    console.log("Mouse over");
+    goal10card.innerHTML = "<p>Reduce inequality within and among countries</p>";
+    goal10card.innerHTML += "<a href='Goals/Goal10.php' class='btn MoreInfo mt-auto'>More Information</a>";
+  });
+
+  goal10card.addEventListener("mouseleave", function(event){
+    console.log("Mouse out");
+    goal10card.innerHTML = "<img class='card-img-top img-fluid' src='Code/php/inc/Images/Goal10.jpg' alt='Goal10' id='goal10'>";
+    var goal10 = document.getElementById("goal10");
+    goal10.addEventListener("mouseover", function(event){
+      console.log("Mouse over");
+      goal10card.innerHTML = "<p>Reduce inequality within and among countries</p>";
+      goal10card.innerHTML += "<a href='Goals/Goal10.php' class='btn MoreInfo mt-auto'>More Information</a>";
+    });
+  });
+  
+  var goal11 = document.getElementById("goal11");
+  var goal11Card = document.getElementById("goal11card");
+  goal11.addEventListener("mouseover", function(event){
+    console.log("Mouse over");
+    goal11card.innerHTML = "<p>Make cities and human settlements inclusive, safe, resilient and sustainable</p>";
+    goal11card.innerHTML += "<a href='Goals/Goal11.php' class='btn MoreInfo mt-auto'>More Information</a>";
+  });
+
+  goal11card.addEventListener("mouseleave", function(event){
+    console.log("Mouse out");
+    goal11card.innerHTML = "<img class='card-img-top img-fluid' src='Code/php/inc/Images/Goal11.jpg' alt='Goal11' id='goal11'>";
+    var goal11 = document.getElementById("goal11");
+    goal11.addEventListener("mouseover", function(event){
+      console.log("Mouse over");
+      goal11card.innerHTML = "<p>Make cities and human settlements inclusive, safe, resilient and sustainable</p>";
+      goal11card.innerHTML += "<a href='Goals/Goal11.php' class='btn MoreInfo mt-auto'>More Information</a>";
+    });
+  });
+
+  var goal12 = document.getElementById("goal12");
+  var goal12Card = document.getElementById("goal12card");
+  goal12.addEventListener("mouseover", function(event){
+    console.log("Mouse over");
+    goal12card.innerHTML = "<p>Ensure sustainable consumption and production patterns</p>";
+    goal12card.innerHTML += "<a href='Goals/Goal12.php' class='btn MoreInfo mt-auto'>More Information</a>";
+  });
+
+  goal12card.addEventListener("mouseleave", function(event){
+    console.log("Mouse out");
+    goal12card.innerHTML = "<img class='card-img-top img-fluid' src='Code/php/inc/Images/Goal12.jpg' alt='Goal12' id='goal12'>";
+    var goal12 = document.getElementById("goal12");
+    goal12.addEventListener("mouseover", function(event){
+      console.log("Mouse over");
+      goal12card.innerHTML = "<p>Ensure sustainable consumption and production patterns</p>";
+      goal12card.innerHTML += "<a href='Goals/Goal12.php' class='btn MoreInfo mt-auto'>More Information</a>";
+    });
+  });
+  
+  var goal13 = document.getElementById("goal13");
+  var goal13Card = document.getElementById("goal13card");
+  goal13.addEventListener("mouseover", function(event){
+    console.log("Mouse over");
+    goal13card.innerHTML = "<p>Take urgent action to combat climate change and its impacts</p>";
+    goal13card.innerHTML += "<a href='Goals/Goal13.php' class='btn MoreInfo mt-auto'>More Information</a>";
+  });
+
+  goal13card.addEventListener("mouseleave", function(event){
+    console.log("Mouse out");
+    goal13card.innerHTML = "<img class='card-img-top img-fluid' src='Code/php/inc/Images/Goal13.jpg' alt='Goal13' id='goal13'>";
+    var goal13 = document.getElementById("goal13");
+    goal13.addEventListener("mouseover", function(event){
+      console.log("Mouse over");
+      goal13card.innerHTML = "<p>Take urgent action to combat climate change and its impacts</p>";
+      goal13card.innerHTML += "<a href='Goals/Goal13.php' class='btn MoreInfo mt-auto'>More Information</a>";
+    });
+  });
+  
+  var goal14 = document.getElementById("goal14");
+  var goal14Card = document.getElementById("goal14card");
+  goal14.addEventListener("mouseover", function(event){
+    console.log("Mouse over");
+    goal14card.innerHTML = "<p>Conserve and sustainably use the oceans, seas and marine resources for sustainable development</p>";
+    goal14card.innerHTML += "<a href='Goals/Goal14.php' class='btn MoreInfo mt-auto'>More Information</a>";
+  });
+
+  goal14card.addEventListener("mouseleave", function(event){
+    console.log("Mouse out");
+    goal14card.innerHTML = "<img class='card-img-top img-fluid' src='Code/php/inc/Images/Goal14.jpg' alt='Goal14' id='goal14'>";
+    var goal14 = document.getElementById("goal14");
+    goal14.addEventListener("mouseover", function(event){
+      console.log("Mouse over");
+      goal14card.innerHTML = "<p>Conserve and sustainably use the oceans, seas and marine resources for sustainable development</p>";
+      goal14card.innerHTML += "<a href='Goals/Goal14.php' class='btn MoreInfo mt-auto'>More Information</a>";
+    });
+  });
+
+  var goal15 = document.getElementById("goal15");
+  var goal15Card = document.getElementById("goal15card");
+  goal15.addEventListener("mouseover", function(event){
+    console.log("Mouse over");
+    goal15card.innerHTML = "<p>Protect, restore and promote sustainable use of terrestrial ecosystems, sustainably manage forests, combat desertification, and halt and reverse land degradation and halt biodiversity loss</p>";
+    goal15card.innerHTML += "<a href='Goals/Goal15.php' class='btn MoreInfo mt-auto'>More Information</a>";
+  });
+
+  goal15card.addEventListener("mouseleave", function(event){
+    console.log("Mouse out");
+    goal15card.innerHTML = "<img class='card-img-top img-fluid' src='Code/php/inc/Images/Goal15.jpg' alt='Goal15' id='goal15'>";
+    var goal15 = document.getElementById("goal15");
+    goal15.addEventListener("mouseover", function(event){
+      console.log("Mouse over");
+      goal15card.innerHTML = "<p>Protect, restore and promote sustainable use of terrestrial ecosystems, sustainably manage forests, combat desertification, and halt and reverse land degradation and halt biodiversity loss</p>";
+      goal15card.innerHTML += "<a href='Goals/Goal15.php' class='btn MoreInfo mt-auto'>More Information</a>";
+    });
+  });
+  
+  var goal16 = document.getElementById("goal16");
+  var goal16Card = document.getElementById("goal16card");
+  goal16.addEventListener("mouseover", function(event){
+    console.log("Mouse over");
+    goal16card.innerHTML = "<p>Promote peaceful and inclusive societies for sustainable development, provide access to justice for all and build effective, accountable and inclusive institutions at all levels</p>";
+    goal16card.innerHTML += "<a href='Goals/Goal16.php' class='btn MoreInfo mt-auto'>More Information</a>";
+  });
+
+  goal16card.addEventListener("mouseleave", function(event){
+    console.log("Mouse out");
+    goal16card.innerHTML = "<img class='card-img-top img-fluid' src='Code/php/inc/Images/Goal16.jpg' alt='Goal16' id='goal16'>";
+    var goal16 = document.getElementById("goal16");
+    goal16.addEventListener("mouseover", function(event){
+      console.log("Mouse over");
+      goal16card.innerHTML = "<p>Promote peaceful and inclusive societies for sustainable development, provide access to justice for all and build effective, accountable and inclusive institutions at all levels</p>";
+      goal16card.innerHTML += "<a href='Goals/Goal16.php' class='btn MoreInfo mt-auto'>More Information</a>";
+    });
+  });
+  
+  var goal17 = document.getElementById("goal17");
+  var goal17Card = document.getElementById("goal17card");
+  goal17.addEventListener("mouseover", function(event){
+    console.log("Mouse over");
+    goal17card.innerHTML = "<p>Strengthen the means of implementation and revitalize the global partnership for sustainable development</p>";
+    goal17card.innerHTML += "<a href='Goals/Goal17.php' class='btn MoreInfo mt-auto'>More Information</a>";
+  });
+
+  goal17card.addEventListener("mouseleave", function(event){
+    console.log("Mouse out");
+    goal17card.innerHTML = "<img class='card-img-top img-fluid' src='Code/php/inc/Images/Goal17.jpg' alt='Goal17' id='goal17'>";
+    var goal17 = document.getElementById("goal17");
+    goal17.addEventListener("mouseover", function(event){
+      console.log("Mouse over");
+      goal17card.innerHTML = "<p>Strengthen the means of implementation and revitalize the global partnership for sustainable development</p>";
+      goal17card.innerHTML += "<a href='Goals/Goal17.php' class='btn MoreInfo mt-auto'>More Information</a>";
     });
   });
 
