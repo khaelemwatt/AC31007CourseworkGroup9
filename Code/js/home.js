579ca18221cf747eff673d4c3621e14260636d7d
Khaelem Watt <kajwatt@dundee.ac.uk>
2022-01-28 14:39:51 +0000

    added all dev goals fucntions

--- Code/js/home.js
+++ Code/js/home.js
@@ -2,15 +2,287 @@ function goal1Hover(){
     cosnole.log("mouse over");
     goal1card.innerHTML = "<h1>No Poverty</h1>";
     goal1card.innerHTML = "<p>End Poverty in all its forms everywhere</p>";
-    goal1card.innerHTML += "<a href='Goals/Goal1.php' class='btn MoreInfo mt-auto'>More Information</a>";
+    goal1card.innerHTML += "<a href='Goals/goal1.php' class='btn MoreInfo mt-auto'>More Information</a>";
   }
-  function addGoal1Hover(){
+  function addgoal1Hover(){
     var goal1 = document.getElementById("goal1");
     var goal1Card = document.getElementById("goal1card");
     goal1.addEventListener("mouseover", goal1Hover());
   }
   function goal1Leave(){
     console.log("Mouse out");
-    goalcard.innerHTML = "<img class='card-img-top img-fluid' src='Code/php/inc/Images/Goal1.jpg' alt='Goal1' id='goal1'>";
-    goal1.addEventListener("mouseover", addGoal1Hover());
+    goalcard.innerHTML = "<img class='card-img-top img-fluid' src='Code/php/inc/Images/goal1.jpg' alt='goal1' id='goal1'>";
+    goal1.addEventListener("mouseover", addgoal1Hover());
+  }
+
+function goal2Hover(){
+    cosnole.log("mouse over");
+    goal2card.innerHTML = "<h1>No Poverty</h1>";
+    goal2card.innerHTML = "<p>End Poverty in all its forms everywhere</p>";
+    goal2card.innerHTML += "<a href='Goals/goal2.php' class='btn MoreInfo mt-auto'>More Information</a>";
+  }
+  function addgoal2Hover(){
+    var goal2 = document.getElementById("goal2");
+    var goal2Card = document.getElementById("goal2card");
+    goal2.addEventListener("mouseover", goal2Hover());
+  }
+  function goal2Leave(){
+    console.log("Mouse out");
+    goalcard.innerHTML = "<img class='card-img-top img-fluid' src='Code/php/inc/Images/goal2.jpg' alt='goal2' id='goal2'>";
+    goal2.addEventListener("mouseover", addgoal2Hover());
+  }
+
+  function goal3Hover(){
+    cosnole.log("mouse over");
+    goal3card.innerHTML = "<h1>No Poverty</h1>";
+    goal3card.innerHTML = "<p>End Poverty in all its forms everywhere</p>";
+    goal3card.innerHTML += "<a href='Goals/goal3.php' class='btn MoreInfo mt-auto'>More Information</a>";
+  }
+  function addgoal3Hover(){
+    var goal3 = document.getElementById("goal3");
+    var goal3Card = document.getElementById("goal3card");
+    goal3.addEventListener("mouseover", goal3Hover());
+  }
+  function goal3Leave(){
+    console.log("Mouse out");
+    goalcard.innerHTML = "<img class='card-img-top img-fluid' src='Code/php/inc/Images/goal3.jpg' alt='goal3' id='goal3'>";
+    goal3.addEventListener("mouseover", addgoal3Hover());
+  }
+
+  function goal4Hover(){
+    cosnole.log("mouse over");
+    goal4card.innerHTML = "<h1>No Poverty</h1>";
+    goal4card.innerHTML = "<p>End Poverty in all its forms everywhere</p>";
+    goal4card.innerHTML += "<a href='Goals/goal4.php' class='btn MoreInfo mt-auto'>More Information</a>";
+  }
+  function addgoal4Hover(){
+    var goal4 = document.getElementById("goal4");
+    var goal4Card = document.getElementById("goal4card");
+    goal4.addEventListener("mouseover", goal4Hover());
+  }
+  function goal4Leave(){
+    console.log("Mouse out");
+    goalcard.innerHTML = "<img class='card-img-top img-fluid' src='Code/php/inc/Images/goal4.jpg' alt='goal4' id='goal4'>";
+    goal4.addEventListener("mouseover", addgoal4Hover());
+  }
+ 
+  function goal5Hover(){
+    cosnole.log("mouse over");
+    goal5card.innerHTML = "<h1>No Poverty</h1>";
+    goal5card.innerHTML = "<p>End Poverty in all its forms everywhere</p>";
+    goal5card.innerHTML += "<a href='Goals/goal5.php' class='btn MoreInfo mt-auto'>More Information</a>";
+  }
+  function addgoal5Hover(){
+    var goal5 = document.getElementById("goal5");
+    var goal5Card = document.getElementById("goal5card");
+    goal5.addEventListener("mouseover", goal5Hover());
+  }
+  function goal5Leave(){
+    console.log("Mouse out");
+    goalcard.innerHTML = "<img class='card-img-top img-fluid' src='Code/php/inc/Images/goal5.jpg' alt='goal5' id='goal5'>";
+    goal5.addEventListener("mouseover", addgoal5Hover());
+  }
+
+  function goal6Hover(){
+    cosnole.log("mouse over");
+    goal6card.innerHTML = "<h1>No Poverty</h1>";
+    goal6card.innerHTML = "<p>End Poverty in all its forms everywhere</p>";
+    goal6card.innerHTML += "<a href='Goals/goal6.php' class='btn MoreInfo mt-auto'>More Information</a>";
+  }
+  function addgoal6Hover(){
+    var goal6 = document.getElementById("goal6");
+    var goal6Card = document.getElementById("goal6card");
+    goal6.addEventListener("mouseover", goal6Hover());
+  }
+  function goal6Leave(){
+    console.log("Mouse out");
+    goalcard.innerHTML = "<img class='card-img-top img-fluid' src='Code/php/inc/Images/goal6.jpg' alt='goal6' id='goal6'>";
+    goal6.addEventListener("mouseover", addgoal6Hover());
+  }
+
+  function goal7Hover(){
+    cosnole.log("mouse over");
+    goal7card.innerHTML = "<h1>No Poverty</h1>";
+    goal7card.innerHTML = "<p>End Poverty in all its forms everywhere</p>";
+    goal7card.innerHTML += "<a href='Goals/goal7.php' class='btn MoreInfo mt-auto'>More Information</a>";
+  }
+  function addgoal7Hover(){
+    var goal7 = document.getElementById("goal7");
+    var goal7Card = document.getElementById("goal7card");
+    goal7.addEventListener("mouseover", goal7Hover());
+  }
+  function goal7Leave(){
+    console.log("Mouse out");
+    goalcard.innerHTML = "<img class='card-img-top img-fluid' src='Code/php/inc/Images/goal7.jpg' alt='goal7' id='goal7'>";
+    goal7.addEventListener("mouseover", addgoal7Hover());
+  }
+
+  function goal8Hover(){
+    cosnole.log("mouse over");
+    goal8card.innerHTML = "<h1>No Poverty</h1>";
+    goal8card.innerHTML = "<p>End Poverty in all its forms everywhere</p>";
+    goal8card.innerHTML += "<a href='Goals/goal8.php' class='btn MoreInfo mt-auto'>More Information</a>";
+  }
+  function addgoal8Hover(){
+    var goal8 = document.getElementById("goal8");
+    var goal8Card = document.getElementById("goal8card");
+    goal8.addEventListener("mouseover", goal8Hover());
+  }
+  function goal8Leave(){
+    console.log("Mouse out");
+    goalcard.innerHTML = "<img class='card-img-top img-fluid' src='Code/php/inc/Images/goal8.jpg' alt='goal8' id='goal8'>";
+    goal8.addEventListener("mouseover", addgoal8Hover());
+  }
+
+  function goal9Hover(){
+    cosnole.log("mouse over");
+    goal9card.innerHTML = "<h1>No Poverty</h1>";
+    goal9card.innerHTML = "<p>End Poverty in all its forms everywhere</p>";
+    goal9card.innerHTML += "<a href='Goals/goal9.php' class='btn MoreInfo mt-auto'>More Information</a>";
+  }
+  function addgoal9Hover(){
+    var goal9 = document.getElementById("goal9");
+    var goal9Card = document.getElementById("goal9card");
+    goal9.addEventListener("mouseover", goal9Hover());
+  }
+  function goal9Leave(){
+    console.log("Mouse out");
+    goalcard.innerHTML = "<img class='card-img-top img-fluid' src='Code/php/inc/Images/goal9.jpg' alt='goal9' id='goal9'>";
+    goal9.addEventListener("mouseover", addgoal9Hover());
+  }
+
+  function goal10Hover(){
+    cosnole.log("mouse over");
+    goal10card.innerHTML = "<h1>No Poverty</h1>";
+    goal10card.innerHTML = "<p>End Poverty in all its forms everywhere</p>";
+    goal10card.innerHTML += "<a href='Goals/goal10.php' class='btn MoreInfo mt-auto'>More Information</a>";
+  }
+  function addgoal10Hover(){
+    var goal10 = document.getElementById("goal10");
+    var goal10Card = document.getElementById("goal10card");
+    goal10.addEventListener("mouseover", goal10Hover());
+  }
+  function goal10Leave(){
+    console.log("Mouse out");
+    goalcard.innerHTML = "<img class='card-img-top img-fluid' src='Code/php/inc/Images/goal10.jpg' alt='goal10' id='goal10'>";
+    goal10.addEventListener("mouseover", addgoal10Hover());
+  }
+
+  function goal11Hover(){
+    cosnole.log("mouse over");
+    goal11card.innerHTML = "<h1>No Poverty</h1>";
+    goal11card.innerHTML = "<p>End Poverty in all its forms everywhere</p>";
+    goal11card.innerHTML += "<a href='Goals/goal11.php' class='btn MoreInfo mt-auto'>More Information</a>";
+  }
+  function addgoal11Hover(){
+    var goal11 = document.getElementById("goal11");
+    var goal11Card = document.getElementById("goal11card");
+    goal11.addEventListener("mouseover", goal11Hover());
+  }
+  function goal11Leave(){
+    console.log("Mouse out");
+    goalcard.innerHTML = "<img class='card-img-top img-fluid' src='Code/php/inc/Images/goal11.jpg' alt='goal11' id='goal11'>";
+    goal11.addEventListener("mouseover", addgoal11Hover());
+  }
+
+  function goal12Hover(){
+    cosnole.log("mouse over");
+    goal12card.innerHTML = "<h1>No Poverty</h1>";
+    goal12card.innerHTML = "<p>End Poverty in all its forms everywhere</p>";
+    goal12card.innerHTML += "<a href='Goals/goal12.php' class='btn MoreInfo mt-auto'>More Information</a>";
+  }
+  function addgoal12Hover(){
+    var goal12 = document.getElementById("goal12");
+    var goal12Card = document.getElementById("goal12card");
+    goal12.addEventListener("mouseover", goal12Hover());
+  }
+  function goal12Leave(){
+    console.log("Mouse out");
+    goalcard.innerHTML = "<img class='card-img-top img-fluid' src='Code/php/inc/Images/goal12.jpg' alt='goal12' id='goal12'>";
+    goal12.addEventListener("mouseover", addgoal12Hover());
+  }
+
+  function goal13Hover(){
+    cosnole.log("mouse over");
+    goal13card.innerHTML = "<h1>No Poverty</h1>";
+    goal13card.innerHTML = "<p>End Poverty in all its forms everywhere</p>";
+    goal13card.innerHTML += "<a href='Goals/goal13.php' class='btn MoreInfo mt-auto'>More Information</a>";
+  }
+  function addgoal13Hover(){
+    var goal13 = document.getElementById("goal13");
+    var goal13Card = document.getElementById("goal13card");
+    goal13.addEventListener("mouseover", goal13Hover());
+  }
+  function goal13Leave(){
+    console.log("Mouse out");
+    goalcard.innerHTML = "<img class='card-img-top img-fluid' src='Code/php/inc/Images/goal13.jpg' alt='goal13' id='goal13'>";
+    goal13.addEventListener("mouseover", addgoal13Hover());
+  }
+
+  function goal14Hover(){
+    cosnole.log("mouse over");
+    goal14card.innerHTML = "<h1>No Poverty</h1>";
+    goal14card.innerHTML = "<p>End Poverty in all its forms everywhere</p>";
+    goal14card.innerHTML += "<a href='Goals/goal14.php' class='btn MoreInfo mt-auto'>More Information</a>";
+  }
+  function addgoal14Hover(){
+    var goal14 = document.getElementById("goal14");
+    var goal14Card = document.getElementById("goal14card");
+    goal14.addEventListener("mouseover", goal14Hover());
+  }
+  function goal14Leave(){
+    console.log("Mouse out");
+    goalcard.innerHTML = "<img class='card-img-top img-fluid' src='Code/php/inc/Images/goal14.jpg' alt='goal14' id='goal14'>";
+    goal14.addEventListener("mouseover", addgoal14Hover());
+  }
+
+  function goal15Hover(){
+    cosnole.log("mouse over");
+    goal15card.innerHTML = "<h1>No Poverty</h1>";
+    goal15card.innerHTML = "<p>End Poverty in all its forms everywhere</p>";
+    goal15card.innerHTML += "<a href='Goals/goal15.php' class='btn MoreInfo mt-auto'>More Information</a>";
+  }
+  function addgoal15Hover(){
+    var goal15 = document.getElementById("goal15");
+    var goal15Card = document.getElementById("goal15card");
+    goal15.addEventListener("mouseover", goal15Hover());
+  }
+  function goal15Leave(){
+    console.log("Mouse out");
+    goalcard.innerHTML = "<img class='card-img-top img-fluid' src='Code/php/inc/Images/goal15.jpg' alt='goal15' id='goal15'>";
+    goal15.addEventListener("mouseover", addgoal15Hover());
+  }
+
+  function goal16Hover(){
+    cosnole.log("mouse over");
+    goal16card.innerHTML = "<h1>No Poverty</h1>";
+    goal16card.innerHTML = "<p>End Poverty in all its forms everywhere</p>";
+    goal16card.innerHTML += "<a href='Goals/goal16.php' class='btn MoreInfo mt-auto'>More Information</a>";
+  }
+  function addgoal16Hover(){
+    var goal16 = document.getElementById("goal16");
+    var goal16Card = document.getElementById("goal16card");
+    goal16.addEventListener("mouseover", goal16Hover());
+  }
+  function goal16Leave(){
+    console.log("Mouse out");
+    goalcard.innerHTML = "<img class='card-img-top img-fluid' src='Code/php/inc/Images/goal16.jpg' alt='goal16' id='goal16'>";
+    goal16.addEventListener("mouseover", addgoal16Hover());
+  }
+
+  function goal17Hover(){
+    cosnole.log("mouse over");
+    goal17card.innerHTML = "<h1>No Poverty</h1>";
+    goal17card.innerHTML = "<p>End Poverty in all its forms everywhere</p>";
+    goal17card.innerHTML += "<a href='Goals/goal17.php' class='btn MoreInfo mt-auto'>More Information</a>";
+  }
+  function addgoal17Hover(){
+    var goal17 = document.getElementById("goal17");
+    var goal17Card = document.getElementById("goal17card");
+    goal17.addEventListener("mouseover", goal17Hover());
+  }
+  function goal17Leave(){
+    console.log("Mouse out");
+    goalcard.innerHTML = "<img class='card-img-top img-fluid' src='Code/php/inc/Images/goal17.jpg' alt='goal17' id='goal17'>";
+    goal17.addEventListener("mouseover", addgoal17Hover());
   }
--- Code/php/inc/home.php
+++ Code/php/inc/home.php
@@ -252,27 +252,7 @@
           </div>
 </body>
 
-<script>
-  
-  function goal1Hover(){
-    cosnole.log("mouse over");
-    goal1card.innerHTML = "<h1>No Poverty</h1>";
-    goal1card.innerHTML = "<p>End Poverty in all its forms everywhere</p>";
-    goal1card.innerHTML += "<a href='Goals/Goal1.php' class='btn MoreInfo mt-auto'>More Information</a>";
-  }
-  function addGoal1Hover(){
-    var goal1 = document.getElementById("goal1");
-    var goal1Card = document.getElementById("goal1card");
-    goal1.addEventListener("mouseover", goal1Hover());
-  }
-  function goal1Leave(){
-    console.log("Mouse out");
-    goal2card.innerHTML = "<img class='card-img-top img-fluid' src='Code/php/inc/Images/Goal2.jpg' alt='Goal2' id='goal2'>";
-    goal2.addEventListener("mouseover", addGoal1Hover());
-  }
-
-  
-</script>
+<script src="Code/js/home.js"></script>
 
 <script src='https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js' integrity='sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj' crossorigin='anonymous'></script>
 <script src='/Code/js/bootstrap.bundle.js' ></script>
