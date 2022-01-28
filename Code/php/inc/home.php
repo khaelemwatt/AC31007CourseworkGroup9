6abfb108e58bf03c798e2a026f0aa81dcad3a6ca
Khaelem Watt <kajwatt@dundee.ac.uk>
2022-01-28 14:26:58 +0000

    readded events to deleted images

--- Code/php/inc/home.php
+++ Code/php/inc/home.php
@@ -261,9 +261,16 @@
     goal1card.innerHTML = "<p>End Poverty in all its forms everywhere</p>";
     goal1card.innerHTML += "<a href='Goals/Goal1.php' class='btn MoreInfo mt-auto'>More Information</a>";
   });
+
   goal1card.addEventListener("mouseleave", function(event){
     console.log("Mouse out");
     goal1card.innerHTML = "<img class='card-img-top img-fluid' src='Code/php/inc/Images/Goal1.jpg' alt='Goal1' id='goal1'>";
+    goal1.addEventListener("mouseover", function(event){
+      console.log("Mouse over");
+      goal1card.innerHTML = "<h1>No Poverty</h1>";
+      goal1card.innerHTML = "<p>End Poverty in all its forms everywhere</p>";
+      goal1card.innerHTML += "<a href='Goals/Goal1.php' class='btn MoreInfo mt-auto'>More Information</a>";
+    });
   });
 
   var goal2 = document.getElementById("goal2");
@@ -273,14 +280,15 @@
     goal2card.innerHTML = "<p>End hunger, achieve food security and improved nutrition and promote sustainable agriculture</p>";
     goal2card.innerHTML += "<a href='Goals/Goal2.php' class='btn MoreInfo align-self-end'>More Information</a>";
   });
+  
   goal2card.addEventListener("mouseleave", function(event){
     console.log("Mouse out");
     goal2card.innerHTML = "<img class='card-img-top img-fluid' src='Code/php/inc/Images/Goal2.jpg' alt='Goal2' id='goal2'>";
     goal2.addEventListener("mouseover", function(event){
-    console.log("Mouse over");
-    goal2card.innerHTML = "<p>End hunger, achieve food security and improved nutrition and promote sustainable agriculture</p>";
-    goal2card.innerHTML += "<a href='Goals/Goal2.php' class='btn MoreInfo align-self-end'>More Information</a>";
-  });
+      console.log("Mouse over");
+      goal2card.innerHTML = "<p>End hunger, achieve food security and improved nutrition and promote sustainable agriculture</p>";
+      goal2card.innerHTML += "<a href='Goals/Goal2.php' class='btn MoreInfo align-self-end'>More Information</a>";
+    });
   });
 
   
