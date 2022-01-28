7eac53822f0fa89dd1a7b0d9cb79cb08e818a7b9
Khaelem Watt <kajwatt@dundee.ac.uk>
2022-01-28 15:38:48 +0000

    changes

--- Code/php/inc/home.php
+++ Code/php/inc/home.php
@@ -20,9 +20,7 @@
     </div>
     <nav class='navbar navbar-dark blackB '>
       <img class='Logo' src='Code/php/inc/Images/SDGlogo.jpg' alt='SDG'>
-      <button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarToggleExternalContent' aria-controls='navbarToggleExternalContent' aria-expanded='false' aria-label='Toggle navigation'>
-        <span class='navbar-toggler-icon'></span>
-      </button>
+      <button>
       
     </nav>
   </div>
