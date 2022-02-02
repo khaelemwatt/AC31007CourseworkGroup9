f2fee3cb250f11d76189a69f8df281012077f336
b0gu5 <91467755+b0gu5@users.noreply.github.com>
2022-02-02 16:24:13 +0000

    removed debug text

--- Code/js/SocialMedia.js
+++ Code/js/SocialMedia.js
@@ -1,21 +1,9 @@
-/* 
-
-Twitter:
-https://twitter.com/share?url=[post-url]&text=[post-title]
-
-
-*/
-
 const twitterBtn = document.querySelector(".twitter-btn");
 
 function init()
 {
-    console.log("test");
-    console.log(document.location.href);
     let postTitle = encodeURI("This is a test goal");
-    console.log(postTitle);
     let postURL = encodeURI(document.location.href);
-    console.log(postURL);
 
     twitterBtn.setAttribute("href", `https://twitter.com/share?url=${postURL}&text=${postTitle}`);
 }
