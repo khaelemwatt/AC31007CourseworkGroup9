4e574b3d12a5560481d4df34df870a9fb6c93b25
b0gu5 <91467755+b0gu5@users.noreply.github.com>
2022-02-02 14:45:23 +0000

    Script to link to social media

---
+++ Code/js/SocialMedia.js
@@ -0,0 +1,17 @@
+/* 
+
+Twitter:
+https://twitter.com/share?url=[post-url]&text=[post-title]
+
+
+*/
+
+const twitterBtn = document.querySelector(".twitter-btn");
+
+function init()
+{
+    let postURL = encodeURI(document.location.href);
+    let postTitle = encodeURI("This is a test goal");
+
+    twitterBtn.setAttribute("href", `https://twitter.com/share?url=${postURL}&text=${postTitle}`);
+}
