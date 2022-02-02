7228b604ea365bfe3abb86c4d16485c031059ce6
b0gu5 <91467755+b0gu5@users.noreply.github.com>
2022-02-02 15:35:16 +0000

    debugging java

--- Code/js/SocialMedia.js
+++ Code/js/SocialMedia.js
@@ -11,8 +11,5 @@ const twitterBtn = document.querySelector(".twitter-btn");
 function init()
 {
     console.log("test");
-    let postURL = encodeURI(document.location.href);
-    let postTitle = encodeURI("This is a test goal");
-
-    twitterBtn.setAttribute("href", `https://twitter.com/share?url=${postURL}&text=${postTitle}`);
+    
 }
