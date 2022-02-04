b71bd834fbf6c441a222bdbacb9f0c18cdf2ef95
Struan Robertson <struanrobertson@protonmail.com>
2022-02-04 13:26:18 +0000

    js file to display correct admin buttons

---
+++ Code/js/loginLevel.js
@@ -0,0 +1,23 @@
+window.sessionStorage;
+
+var nav = document.getElementById("navbar");
+console.log(sessionStorage.getItem("username"));
+console.log(sessionStorage.getItem("level"));
+
+if(sessionStorage.getItem("username") != null){
+    if(sessionStorage.getItem("level") == 0){
+    nav.innerHTML += '<a href="./createAdmin.html" class="btn btn-info" role="button">Create Admin</a>';
+    }
+    if(sessionStorage.getItem("level") <= 1){      
+    nav.innerHTML += '<a href="./editEvent.html" class="btn btn-info" role="button">Edit Event</a>';
+    nav.innerHTML += '<a href="./createEvent.html" class="btn btn-info" role="button">Create Event</a>'; 
+    nav.innerHTML += '<a href="./CreateTour.html" class="btn btn-info" role="button">Create tour</a>'; 
+    nav.innerHTML += '<a href="./viewTours.html" class="btn btn-info" role="button">view tour</a>';
+    nav.innerHTML += '<a href="./suggestions.html class="btn btn-info role="button"Suggestions</a>';
+    }
+    if(sessionStorage.getItem("level") <=2){
+    //Placeholder for now
+    }    
+}else{    
+    nav.innerHTML += '<a href="./login.html" class="btn btn-info" role="button">Login</a>';
+}
