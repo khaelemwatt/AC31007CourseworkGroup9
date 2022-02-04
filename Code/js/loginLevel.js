84dc503eb64ba30f9d54b94d8b15786a36efbb0b
Struan Robertson <struanrobertson@protonmail.com>
2022-02-04 13:29:59 +0000

    logout button

--- Code/js/loginLevel.js
+++ Code/js/loginLevel.js
@@ -14,10 +14,15 @@ if(sessionStorage.getItem("username") != null){
     nav.innerHTML += '<a href="./CreateTour.html" class="btn btn-info" role="button">Create tour</a>'; 
     nav.innerHTML += '<a href="./viewTours.html" class="btn btn-info" role="button">view tour</a>';
     nav.innerHTML += '<a href="./suggestions.html class="btn btn-info role="button"Suggestions</a>';
+    nav.innerHTML += '<a href="#" class="btn btn-info" role="button" onclick="logOut();">Log Out></a>;'
     }
     if(sessionStorage.getItem("level") <=2){
     //Placeholder for now
     }    
 }else{    
     nav.innerHTML += '<a href="./login.html" class="btn btn-info" role="button">Login</a>';
+}
+
+function logOut() {
+    sessionStorage.clear();
 }
